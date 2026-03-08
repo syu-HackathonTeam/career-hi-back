f060e86b28c8291c01538e6ebb2ce81502431288
Wangjonghui <linkwjh61@gmail.com>
2026-03-08 18:07:50 +0900

    CORS 문제 해결

--- career-hi-api/src/main/java/com/careerhi/api/global/config/SecurityConfig.java
+++ career-hi-api/src/main/java/com/careerhi/api/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.careerhi.api.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,18 +29,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         // 로그인, 회원가입, H2 콘솔은 허용
-                        .requestMatchers("/api/v1/auth/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/", "/api/v1/auth/**", "/h2-console/**").permitAll()
                         // 그 외 모든 요청은 인증(토큰) 필요!
                         .anyRequest().authenticated()
                 )
                 // ★ JWT 필터 추가: UsernamePasswordAuthenticationFilter 앞에서 미리 검사
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()));
 
         return http.build();
